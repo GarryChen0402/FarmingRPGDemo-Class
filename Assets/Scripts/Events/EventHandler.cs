1c15e23c4e1cfa32d58bc748c743e0e0af345b2d
GarryChen0402 <gychen0402@foxmail.com>
2025-12-20 21:54:11 +0800

    Class 04: Events and event handler

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,31 @@
+﻿public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, 
+    bool isRunning, bool isIdle, bool isCarrying, ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingTooLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingToolRight, bool isSwingToolLeft, bool isSwingToolUp, bool isSwingToolDown,
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight);
+
+
+public static class EventHandler
+{
+    //Movement Event
+    public static event MovementDelegate MovementEvent;
+    
+    //Movement Event Call For Publishers
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking,
+        bool isRunning, bool isIdle, bool isCarrying, ToolEffect toolEffect,
+        bool isUsingToolRight, bool isUsingTooLeft, bool isUsingToolUp, bool isUsingToolDown,
+        bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+        bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+        bool isSwingToolRight, bool isSwingToolLeft, bool isSwingToolUp, bool isSwingToolDown,
+        bool idleUp, bool idleDown, bool idleLeft, bool idleRight)
+    {
+        if (MovementEvent != null)
+            MovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying, toolEffect,
+                isUsingToolRight, isUsingTooLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown, isPickingRight,
+                isPickingLeft, isPickingUp, isPickingDown, isSwingToolRight, isSwingToolLeft, isSwingToolUp,
+                isSwingToolDown, idleUp, idleDown, idleLeft, idleRight);
+    }
+}
