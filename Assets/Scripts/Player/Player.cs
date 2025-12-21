5299394dfc7b99b306e53de12385a26cbbba1d18
GarryChen0402 <gychen0402@foxmail.com>
2025-12-21 16:48:51 +0800

    Class 06: Basic Player Movement(Part 2)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,38 +1,39 @@
+using System;
 using UnityEngine;
 
 public class Player : SingletonMonoBehaviour<Player>
 {
     //Movement Parameters
-    private  float xInput;
-    private  float yInput;
+    private float xInput;
+    private float yInput;
     private bool isIdle;
     private bool isCarrying;
-    private  bool isWalking;
-    private  bool isRunning;
-    private  ToolEffect toolEffect = ToolEffect.none;
-    private  bool isUsingToolRight;
-    private  bool isUsingToolLeft;
-    private  bool isUsingToolUp;
-    private  bool isUsingToolDown;
-    private  bool isLiftingToolRight;
-    private  bool isLiftingToolLeft;
-    private  bool isLiftingToolUp;
-    private  bool isLiftingToolDown;
-    private  bool isSwingingToolRight;
-    private  bool isSwingingToolLeft;  
-    private  bool isSwingingToolUp;
-    private  bool isSwingingToolDown;
-    private  bool isPickingRight;
-    private  bool isPickingLeft;
-    private  bool isPickingUp;
-    private  bool isPickingDown;
-    private  bool idleUp;
-    private  bool idleDown;
-    private  bool idleLeft;
-    private  bool idleRight;
+    private bool isWalking;
+    private bool isRunning;
+    private ToolEffect toolEffect = ToolEffect.none;
+    private bool isUsingToolRight;
+    private bool isUsingToolLeft;
+    private bool isUsingToolUp;
+    private bool isUsingToolDown;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolUp;
+    private bool isLiftingToolDown;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolUp;
+    private bool isSwingingToolDown;
+    private bool isPickingRight;
+    private bool isPickingLeft;
+    private bool isPickingUp;
+    private bool isPickingDown;
+    private bool idleUp;
+    private bool idleDown;
+    private bool idleLeft;
+    private bool idleRight;
 
     private Rigidbody2D rigidbody2D;
-    
+
     private Direction playerDirection;
 
     private float movementSpeed;
@@ -48,7 +49,108 @@ public class Player : SingletonMonoBehaviour<Player>
     protected override void Awake()
     {
         base.Awake();
-        
-        rigidbody2D = GetComponent<Rigidbody2D>(); 
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        #region Player Input
+
+        ResetAnimationTriggers();
+
+        PlayerMovementInputs();
+
+        PlayerWalkInput();
+
+        EventHandler.CallMovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying, toolEffect,
+            isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown, isPickingRight,
+            isPickingLeft, isPickingUp, isPickingDown, isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp,
+            isSwingingToolDown, false, false, false, false);
+
+        #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 move = new Vector2(xInput * movementSpeed *  Time.deltaTime, yInput * movementSpeed * Time.deltaTime);
+
+        rigidbody2D.MovePosition(rigidbody2D.position + move);
+    }
+
+    private void PlayerWalkInput()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isRunning = false;
+            isWalking = true;
+            isIdle = false;
+            movementSpeed = Settings.WalkingSpeed;
+        }
+        else
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.RunningSpeed;
+        }
+    }
+
+    private void ResetAnimationTriggers()
+    {
+        isUsingToolRight = false;
+        isUsingToolLeft = false;
+        isUsingToolUp = false;
+        isUsingToolDown = false;
+        isLiftingToolRight = false;
+        isLiftingToolLeft = false;
+        isLiftingToolUp = false;
+        isLiftingToolDown = false;
+        isSwingingToolRight = false;
+        isSwingingToolLeft = false;
+        isSwingingToolUp = false;
+        isSwingingToolDown = false;
+        isPickingRight = false;
+        isPickingLeft = false;
+        isPickingUp = false;
+        isPickingDown = false;
+        toolEffect = ToolEffect.none;
+    }
+
+    private void PlayerMovementInputs()
+    {
+        yInput = Input.GetAxisRaw("Vertical");
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (yInput != 0 && xInput != 0)
+        {
+            xInput = xInput * 0.71f;
+            yInput = yInput * 0.71f;
+        }
+
+        if (xInput != 0 || yInput != 0)
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.RunningSpeed;
+
+            if (xInput < 0) playerDirection = Direction.left;
+            else if (xInput > 0) playerDirection = Direction.right;
+            else if (yInput < 0) playerDirection = Direction.down;
+            else playerDirection = Direction.up;
+        }
+        else if (xInput == 0 && yInput == 0)
+        {
+            isRunning = false;
+            isWalking = false;
+            isIdle = true;
+        }
     }
 }
